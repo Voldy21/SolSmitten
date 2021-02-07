40d18ebd5f9877a6e5aa9e2c2392eca6a3e994a3
Voldy21 <jonahlvngstn@gmail.com>
2021-02-07 12:15:44 -0500

    fix bugs

--- application.py
+++ application.py
@@ -3,11 +3,13 @@ import ss_db as db
 
 application = Flask(__name__)
 
+
 @application.route("/")
 def hello():
     details = db.get_details()
     print(details)
     return "Hello World"
+
 
 if __name__ == "__main__":
     application.run()
