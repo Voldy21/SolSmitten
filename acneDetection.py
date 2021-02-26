6e96a6b1b1924a6ab63794bfad81aa71c19ce632
Nicolas Serna <sernan100@gmail.com>
2021-02-25 21:18:46 -0500

    added test

--- acneDetection.py
+++ acneDetection.py
@@ -1,1 +1,13 @@
-print("test")
+model.save('model_weights.pth')
+# Specify the path to your image
+image = utils.read_image('testImages/testImage2.png')
+predictions = model.predict(image)
+
+# predictions format: (labels, boxes, scores)
+labels, boxes, scores = predictions
+
+print(labels) 
+
+print(boxes)
+
+print(scores)
